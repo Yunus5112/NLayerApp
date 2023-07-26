0c6ce37280f0d7be88aeeaee080776a0502c039b
yunus.solak <yunus.solak@sendeo.com.tr>
2023-07-26 12:24:12 +0300

    Add project files.

---
+++ NLayerService/Services/ProductServiceWithNoCaching.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using NLayerCore.DTOs;
+using NLayerCore.Models;
+using NLayerCore.Repositories;
+using NLayerCore.Services;
+using NLayerCore.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerService.Services
+{
+    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+
+            _mapper = mapper;
+            _productRepository = productRepository;
+
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        {
+            var products = await _productRepository.GetProductsWithCategory();
+            var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+
+        }
+    }
+}
